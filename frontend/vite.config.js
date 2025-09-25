d5cea11924bfbdd872f1aba2bbe706aa8ba82f6a
Ritam chatterjee <ritamchatterjee987@gmail.com>
2025-09-25 15:56:49 +0530

    somthing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
-   import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
-   // https://vitejs.dev/config/
-   export default defineConfig({
-     plugins: [react()],
-     build: {
-       outDir: 'dist',
-       assetsDir: 'assets',
-       rollupOptions: {
-         input: './public/index.html' 
-       },
-     },
-     server: {
-       port: 5173,
-       host: true
-     }
-   })
-   
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: './public/index.html'
+    },
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
+})
