53fb10515216864f007ad42e3e2f3b0b8562bf21
Ritam chatterjee <ritamchatterjee987@gmail.com>
2025-09-30 19:03:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,9 @@ app.use(express.json());
 // Serve uploaded images statically
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+
+
+
 // Routes
 const authRoutes = require("./routes/auth");
 const blogRoutes = require("./routes/blogs");
@@ -37,3 +40,4 @@ app.get("/", (req, res) => res.send("E-Cell Blogging Backend is running!"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
