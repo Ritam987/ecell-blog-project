149dc0a3a8b696e7ba1cefadfa51d95ae0cb854f
Ritam chatterjee <ritamchatterjee987@gmail.com>
2025-10-02 22:07:58 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,12 +43,6 @@ mongoose
 
 
 
-// Serve React static files
-app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
-});
 
 
 // Default route
@@ -57,5 +51,6 @@ app.get("/", (req, res) => res.send("E-Cell Blogging Backend is running!"));
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
