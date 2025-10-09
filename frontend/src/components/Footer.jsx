e7a9fafa2d09b0770ededb317940be721543ffb1
Ritam chatterjee <ritamchatterjee987@gmail.com>
2025-10-09 10:32:01 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-600 text-white p-4 text-center">
+    <footer className="bg-darkBg text-neonBlue p-4 text-center shadow-neon mt-8">
       &copy; {new Date().getFullYear()} E-Cell Blogging Website
     </footer>
   );
