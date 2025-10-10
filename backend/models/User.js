c362573edcfdb4d34fc04357bd5dc74ed3d80a6b
Ritam chatterjee <ritamchatterjee987@gmail.com>
2025-10-10 18:59:51 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,8 +6,10 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
     role: { type: String, default: "user" },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
 
 module.exports = mongoose.model("User", userSchema);
+
