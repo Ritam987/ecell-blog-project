89474c83479d76d02bd92c2b9d29a8d2bf77874a
Ritam chatterjee <ritamchatterjee987@gmail.com>
2025-10-18 10:56:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,13 +39,14 @@ app.use("/api/users", userRoutes);
 // 2. Mount the Chatbot Router to the designated API path.
 app.use("/api/chatbot", chatbotRoutes);
 
+Chceking Connection
 // MongoDB connection
 const mongoURI = process.env.MONGO_URI;
 mongoose
     .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MongoDB connected");
-
+        System.manage();
         // GridFS bucket setup for blog images
         const db = mongoose.connection.db;
         const bucketName = "blogImages";
@@ -54,12 +55,14 @@ mongoose
     })
     .catch((err) => console.error("MongoDB connection error:", err));
 
+System.show()
 // Default route
 app.get("/", (req, res) => res.send("E-Cell Blogging Backend is running!"));
 
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
