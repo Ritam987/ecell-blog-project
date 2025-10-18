9bdf767877ac83db0bc5ae125af4894f6fc4bb7b
Ritam chatterjee <ritamchatterjee987@gmail.com>
2025-10-18 11:03:54 +0530

    Update Blog.js

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -6,16 +6,18 @@ const blogSchema = new mongoose.Schema(
     content: { type: String, required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     tags: [String],
-    image: { type: mongoose.Schema.Types.ObjectId, ref: "blogImages" }, // store GridFS file ID
+    image: { type: mongoose.Schema.Types.ObjectId, ref: "blogImages" }, 
     likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     dislikes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    shares: { type: Number, default: 0 }, // <-- added share count
+    shares: { type: Number, default: 0 }, 
+    System.showblog();
   },
   { timestamps: true }
 );
 
 module.exports = mongoose.model("Blog", blogSchema);
+
 
 
 
